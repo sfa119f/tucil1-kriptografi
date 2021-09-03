06c537ff09743366e7aab18a95ff0bcd44cc6cbd
farisfadhilah27 <farisfadhilah27@gmail.com>
2021-09-03 12:45:21 +0700

    fix standard & auto vigenere

--- autoVigenereCipher.py
+++ autoVigenereCipher.py
@@ -1,53 +1,54 @@
-import random
-
 def makePlain(PlainText):
+# Mengubah plaintext sesuai dengan format huruf kapital dan membuang angka, spasi, dan tanda baca
     p = []
     plainText = PlainText.upper()
-    plain = list(plainText)
+    plain = list(plainText) # Memecah kata menjadi huruf-huruf
     for char in plain:
         if (char.isalpha()):
             p.append(char)
-        newP = "".join(p)
+        newP = "".join(p) # Menggabungkan huruf menjadi kata
     return newP
 
 def makeKeyAuto(plainText, key):
+# Mengubah key sesuai dengan format huruf besar dan menambah plainText ke belakang key hingga panjang key = plainText
     key = key.upper()
-    key = list(key)
+    key = list(key) # Memecah kata menjadi huruf-huruf
     if (len(plainText) == len(key)):
         return key
     else:
         for i in range (len(plainText)-len(key)):
             k = plainText[i%len(plainText)] # Membuat perulangan key sepanjang plainText
             key.append(k)
-            newKey = "".join(key)
+            newKey = "".join(key) # Menggabungkan huruf menjadi kata
         return newKey
 
-def cipherVigenere(plainText, key):
+def autoCipher(plainText, key):
+# Melakukan enkripsi plainText menjadi cipherText
+# Formula : c[j] = E(p[j])  = (p[j] + k[i]) mod 26
+    p = makePlain(plainText)
+    k = makeKeyAuto(p,key)
     cipher = []
-    for i in range (len(plainText)):
-        n = (ord(plainText[i]) + ord(key[i])) % 26
-        n += ord('A')
-        cipher.append(chr(n))
-        cip = "".join(cipher)
+    for i in range (len(p)):
+        n = ((ord(p[i]) + ord(k[i])) % 26) + ord('A') # konversi ke angka
+        cipher.append(chr(n)) # konversi ke alfabet
+        cip = "".join(cipher) # Menggabungkan huruf menjadi kata
     return cip
 
-def plainVigenere(cipherText, key):
+def autoPlain(cipherText, plainText,key):
+# Melakukan deskripsi cipherText menjadi plainText
+# Formula : p[j] = D(c[j])  = (c[j] - k[i]) mod 26
+    p = makePlain(plainText)
+    k = makeKeyAuto(p,key)
     plain = []
     for i in range(len(cipherText)):
-        n = (ord(cipherText[i]) - ord(key[i]) + 26) % 26
-        n += ord ('A')
-        plain.append(chr(n))
-        pl = "".join(plain)
+        n = ((ord(cipherText[i]) - ord(k[i]) + 26) % 26) + ord ('A') # konversi ke angka
+        plain.append(chr(n)) # konversi ke alfabet
+        pl = "".join(plain) # Menggabungkan huruf menjadi kata
     return pl
 
-a = "negara penghasil minyak"
-b = "INDO"
+a = "thisplaintext"
+b = "SONY"
+c = "LVVQISIACEEFG"
 
-c = makePlain(a)
-k = makeKeyAuto(c,b)
-print(a)
-print(k)
-h = cipherVigenere(c,k)
-print("Cipher Text: " + h)
-p = plainVigenere(h,k)
-print("Plain Text: " + p)
+print(autoCipher(a,b))
+print(autoPlain(c,a,b))
--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,53 +1,53 @@
-import random
-
 def makePlain(PlainText):
+# Mengubah plaintext sesuai dengan format huruf kapital dan membuang angka, spasi, dan tanda baca
     p = []
     plainText = PlainText.upper()
-    plain = list(plainText)
+    plain = list(plainText) # Memecah kata menjadi huruf-huruf
     for char in plain:
         if (char.isalpha()):
             p.append(char)
-        newP = "".join(p)
+        newP = "".join(p) # Menggabungkan huruf menjadi kata
     return newP
 
 def makeKey(plainText,key):
+# Mengubah key sesuai dengan format huruf besar dan mengulang char pada key sehingga panjang key = plaintext
     key = key.upper()
-    key = list(key) # Memecah kata menjadi huruf-huruf dalam array
+    key = list(key) # Memecah kata menjadi huruf-huruf
     if (len(plainText) == len(key)): 
         return key
     else:
         for i in range (len(plainText)-len(key)):
-            k = key[i%len(key)] # Membuat perulangan key sepanjang plainText
+            k = key[i%len(key)] 
             key.append(k)
-            newKey = "".join(key)
+            newKey = "".join(key) # Menggabungkan huruf menjadi kata
         return newKey
 
-def cipherVigenere(plainText, key):
+def vigenereCipher(plainText, key):
+# Melakukan enkripsi plainText menjadi cipherText
+# Formula : c[j] = E(p[j])  = (p[j] + k[i]) mod 26
+    p = makePlain(plainText)
+    k = makeKey(p,key)
     cipher = []
-    for i in range (len(plainText)):
-        n = (ord(plainText[i]) + ord(key[i])) % 26
-        n += ord('A')
-        cipher.append(chr(n))
-        cip = "".join(cipher)
+    for i in range (len(p)):
+        n = ((ord(p[i]) + ord(k[i])) % 26) + ord('A') # konversi ke angka
+        cipher.append(chr(n)) # konversi ke alfabet
+        cip = "".join(cipher) # Menggabungkan huruf menjadi kata
     return cip
 
-def plainVigenere(cipherText, key):
+def vigenerePlain(cipherText, key):
+# Melakukan deskripsi cipherText menjadi plainText
+# Formula : p[j] = D(c[j])  = (c[j] - k[i]) mod 26
+    k = makeKey(cipherText,key)
     plain = []
     for i in range(len(cipherText)):
-        n = (ord(cipherText[i]) - ord(key[i]) + 26) % 26
-        n += ord ('A')
-        plain.append(chr(n))
-        pl = "".join(plain)
+        n = ((ord(cipherText[i]) - ord(k[i]) + 26) % 26) + ord ('A') # konversi ke angka
+        plain.append(chr(n)) # konversi ke alfabet
+        pl = "".join(plain) # Menggabungkan huruf menjadi kata
     return pl
 
 a = "thisplaintext"
 b = "SONY"
+c = "LVVQHZNGFHRVL"
 
-c = makePlain(a)
-k = makeKey(c,b)
-print(a)
-print(k)
-h = cipherVigenere(c,k)
-print("Cipher Text: " + h)
-p = plainVigenere(h,k)
-print("Plain Text: " + p)
+print(vigenereCipher(a,b))
+print(vigenerePlain(c,b))
